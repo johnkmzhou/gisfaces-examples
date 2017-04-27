d0738a1f9cfa256fd771c4312ac7dee6e7577708
John Zhou <john.km.zhou@gmail.com>
2017-04-27 02:05:31 -1000

    Initial commit

---
+++ src/main/java/com/gisfaces/examples/ServicesBean.java
@@ -0,0 +1,56 @@
+package com.gisfaces.examples;
+
+import javax.enterprise.inject.Model;
+import javax.faces.event.ActionEvent;
+
+import com.gisfaces.model.service.ServiceDirectoryMetadata;
+import com.gisfaces.model.service.ServiceDirectoryMetadataBuilder;
+import com.gisfaces.utilities.JSFUtilities;
+
+@Model
+public class ServicesBean {
+
+    private String url;
+    private ServiceDirectoryMetadata serviceDirectory;
+
+    public ServicesBean() {
+        reset();
+    }
+
+    public void reset() {
+        this.url = "http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/PublicSafety";
+        this.serviceDirectory = null;
+    }
+
+    public void doQueryButtonActionListener(ActionEvent event) {
+        System.out.println("Begin query action listener.");
+
+        try {
+            this.serviceDirectory = null;
+
+            ServiceDirectoryMetadataBuilder builder = new ServiceDirectoryMetadataBuilder();
+            this.serviceDirectory = builder.build(this.url);
+        } catch (Exception e) {
+            e.printStackTrace();
+            JSFUtilities.addErrorMessage(e.getMessage());
+        }
+
+        System.out.println("End query action listener.");
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public ServiceDirectoryMetadata getServiceDirectory() {
+        return serviceDirectory;
+    }
+
+    public void setServiceDirectory(ServiceDirectoryMetadata serviceDirectory) {
+        this.serviceDirectory = serviceDirectory;
+    }
+}
